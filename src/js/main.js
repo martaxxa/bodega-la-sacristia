089428d71d6e6a56e8283f7cd4c6c6c7274f41f0
martaxxa <mchacartegui29@gmail.com>
2025-03-21 11:26:06 +0100

    Add javascript and menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-console.log('>> Ready :)');
+document.addEventListener("DOMContentLoaded", () => {
+
+    //Query
+  const menuToggle = document.querySelector(".js_hamburgerMenu");
+  const menu = document.querySelector(".js_menu");
+  const header = document.querySelector(".js_header");
+
+  //Menú desplegable
+  if (menuToggle && menu) {
+    menuToggle.addEventListener("mouseenter", (event) => {
+      menu.classList.add("menu--active");
+    });
+
+    menu.addEventListener("mouseleave", (event) => {
+      menu.classList.remove("menu--active");
+    });
+
+    menu.addEventListener("mouseenter", (event) => {
+      event.stopPropagation();
+    });
+
+    menu.addEventListener("mouseleave", () => {
+      menu.classList.remove("menu--active");
+    });
+  } else {
+    console.error("Uno o más elementos no se encontraron en el DOM");
+  }
+
+  //Fondo del header al hacer scroll
+  const scrollThreshold = 50;
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > scrollThreshold) {
+      header.classList.add('header--scrolled');
+    } else {
+      header.classList.remove('header--scrolled');
+    }
+  });
+});
